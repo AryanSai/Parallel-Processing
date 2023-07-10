9f8d88d40b7d95b8578659f705529df4f906b6e9
Aryan Sai <aryansaiarvapelly@gmail.com>
2023-07-10 21:24:44 +0530

    first commit

---
+++ HelloOMP.c
@@ -0,0 +1,21 @@
+// command to run ===> gcc -fopenmp HelloWorld.c
+// export OMP_NUM_THREADS=8
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int tid;
+#pragma omp parallel private(tid) num_threads(5) // each thread has its own private copy
+    {
+        tid = omp_get_thread_num();
+        printf("Hello World... from thread  %d !\n", tid);
+
+        if (tid == 0)
+        { // parent
+            printf("I am the parent!!\n");
+            printf("Number of threads = %d\n", omp_get_num_threads());
+        }
+    }
+}
